d14ab8eb3f1fa34c2a25d2f132e8587af04e5304
Dharshan2208 <dharshan080622@gmail.com>
2025-08-08 21:42:56 +0530

    Day 08-08-2025 Minstack done using another approach

---
+++ 0155.java
@@ -0,0 +1,88 @@
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.pop();
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
+
+
+//  Another Implementation can be where we can make our own node and evrything
+// like we will change the node definition and use Math.min for getting the min
+// value
+
+
+// class MinStack {
+//     private Node head;
+
+//     public void push(int x) {
+//         if (head == null)
+//             head = new Node(x, x, null);
+//         else
+//             head = new Node(x, Math.min(x, head.min), head);
+//     }
+
+//     public void pop() {
+//         head = head.next;
+//     }
+
+//     public int top() {
+//         return head.val;
+//     }
+
+//     public int getMin() {
+//         return head.min;
+//     }
+
+//     private class Node {
+//         int val;
+//         int min;
+//         Node next;
+
+//         private Node(int val, int min, Node next) {
+//             this.val = val;
+//             this.min = min;
+//             this.next = next;
+//         }
+//     }
+// }
