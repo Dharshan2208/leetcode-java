f751b1aec88ee9199820ebf29adbd2516cfbe358
Dharshan2208 <dharshan080622@gmail.com>
2025-08-09 21:57:24 +0530

    Day 09-08-2025 Problem 234 done in O(1) space

--- 0234.java
+++ 0234.java
@@ -34,3 +34,54 @@ class Solution {
 
 
 // Have to think how to do it in O(n) time and O(1) space
+// done using reversing the ll and then comparing
+
+class Solution {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // returns first half end
+        ListNode middle = findMiddle(head);
+        ListNode secondHalfStart = reverse(middle.next);
+
+        ListNode firstHalfStart = head;
+
+        while (secondHalfStart != null) {
+            if (firstHalfStart.val != secondHalfStart.val) {
+                return false;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+
+        return true;
+    }
+
+}
