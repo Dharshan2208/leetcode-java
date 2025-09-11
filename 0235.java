636debe694795f5e468ca99176d6a966ef2e9338
Dharshan2208 <dharshan080622@gmail.com>
2025-09-11 23:08:05 +0530

    Day 11-09-2025 Did BT questions for timepass

---
+++ 0235.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Did the same as LCA for BT but will do for BST
+        // if(root == null || root == p || root ==q){
+        // return root;
+        // }
+
+        // TreeNode left = lowestCommonAncestor(root.left,p,q);
+        // TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        // if(left == null){
+        // return right;
+        // }
+        // else if(right == null){
+        // return left;
+        // }
+        // else{
+        // return root;
+        // }
+
+        // BST logic
+        while (root != null) {
+            if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
